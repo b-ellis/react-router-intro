ab8739fd97a2c9fdb3d2ef4079d99761712be615
b-ellis <bradjellis18@gmail.com>
2016-12-03 18:19:32 -0800

    first commit

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
 require('babel-polyfill');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	ReactDOM.render(document.getElementbyId('app'));
+})
 
