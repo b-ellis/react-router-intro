5fa2d25071f32c6104ef11ad09db2b4e96fb830d
b-ellis <bradjellis18@gmail.com>
2016-12-03 19:07:46 -0800

    first route added

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
 require('babel-polyfill');
-import React from 'react';
-import ReactDOM from 'react-dom';
+var React = require('react');
+var ReactDOM = require('react-dom');
+var router = require('react-router');
+var Router = router.Router;
+var Route = router.Route;
+var hashHistory = router.hashHistory;
 
+var ContactListContainer = require('./components/contact-list-container');
+
+var routes = (
+    <Router history={hashHistory}>
+        <Route path="/contacts" component={ContactListContainer} />
+    </Router>
+);
 
 document.addEventListener('DOMContentLoaded', function() {
-	ReactDOM.render(document.getElementbyId('app'));
-})
-
+    ReactDOM.render(routes, document.getElementById('app'));
+});
